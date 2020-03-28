c573565b73f14293fb8bc6ef3ffe30d24efe967a
micher94 <55001134+micher94@users.noreply.github.com>
2020-03-28 12:01:30 -0400

    adding test file

---
+++ test_script.py
@@ -0,0 +1,9 @@
+from Homework4 import main 
+import pytest
+
+def test_values():
+	pass = "done"
+	assert convert_numeral(2150) == pass
+	assert convert_numeral(2123) == pass
+	assert convert_numeral(-1023) == pass
+	assert convert_numeral(1.2) == pass

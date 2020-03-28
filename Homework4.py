ae4d84869c6f9e51de92444fcbc2d4a47cc0a3e8
micher94 <55001134+micher94@users.noreply.github.com>
2020-03-28 11:47:24 -0400

    compatible with flask
    this is the version that is compatible with flask

---
+++ Homework4.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from makeimage import makeimage
+from readtweets import readtweets
+from Queue import Queue
+import configparser
+import subprocess
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/starthere/<keyword>')
+def main(keyword):
+
+	# get keys
+	# config = configparser.ConfigParser()
+	# config.read('C:/Users/micha/Documents/Spring2019/EC500/Homework4/keys')
+	# print(config.get('auth','consumer_key').split())
+
+	consumer_key = '8cRGDF5RPNboNrR8JaswNySuC'
+	consumer_secret = 'BSX2gAzfqtHDw1cyELlapdomr96qy7Y1pn3D06koT8ZOixcpsu'
+	access_token = '1172138619680673794-fGznWzLf465gsVed1SwI6kPPIqHikj'
+	access_secret = 'TXb8K6iZ67MHoC03z5SgwBnmeOv3tajqbZqqd3QrT8CXb'
+
+	q = Queue()
+
+	inputlist = keyword
+	inputlist = inputlist.split(',')
+
+	for words in inputlist:
+		q.queueup(words)
+
+	q.queuelist()
+	print(q.queuelength())
+
+	i = 0
+	filename = str(i) + "-%01d.png"
+	videoname = keyword+".avi"
+	while q.queuelength()>0:
+		tweets = readtweets(q.items[0],consumer_key,consumer_secret)
+		j = 0
+		for tweet in tweets:
+			print(tweet)
+			makeimage(i,j,tweet)
+			j=j+1
+		q.queuedown()
+		q.queuelist()
+		i = i+1
+		print("I am here")
+		subprocess.call(['ffmpeg', '-framerate', '.1', '-i', filename, videoname])
+	return "done"
+
+
+
+if __name__ == '__main__':
+    main()
--- makeimage.py
+++ makeimage.py
@@ -1,13 +1,13 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
-def makeimage(num,tweet):
+def makeimage(num1,num2,tweet):
 	tweet=tweet.encode('utf-8') 
 	font = ImageFont.load_default()
 	# https://code-maven.com/create-images-with-python-pil-pillow
-	filename = str(num) + ".png"
-	image = Image.new(mode = "RGB", size = (100,100), color = 'blue')
+	filename = str(num1) + "-" + str(num2) + ".png"
+	image = Image.new(mode = "RGB", size = (1000,100), color = 'black')
 	tweeter = ImageDraw.Draw(image)
-	tweeter.text((0,0),tweet,fill=(255,255,0),spacing=10,align='center',stroke_width=1)
+	tweeter.text((0,50),tweet,fill=(255,255,0),spacing=250,align='center')
 
 	image.save(filename)

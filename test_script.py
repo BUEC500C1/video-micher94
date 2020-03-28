b0652bd091b6b955a21dbe1c32f61ecd39622e37
micher94 <55001134+micher94@users.noreply.github.com>
2020-03-28 12:09:21 -0400

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -2,7 +2,7 @@ from Homework4 import main
 import pytest
 
 def test_values():
-	assert main(Boston) == "Boston"
-	assert main(school) == "school"
-	assert main(family) == "family"
-	assert main(coffee) == "coffee"
+	assert main("Boston") == "Boston"
+	assert main("school") == "school"
+	assert main("family") == "family"
+	assert main("coffee") == "coffee"

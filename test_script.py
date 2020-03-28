728012307db8c133264ae847051ab908e9fe09b1
micher94 <55001134+micher94@users.noreply.github.com>
2020-03-28 12:05:22 -0400

    changing test values

--- test_script.py
+++ test_script.py
@@ -2,8 +2,7 @@ from Homework4 import main
 import pytest
 
 def test_values():
-	pass = "done"
-	assert convert_numeral(2150) == pass
-	assert convert_numeral(2123) == pass
-	assert convert_numeral(-1023) == pass
-	assert convert_numeral(1.2) == pass
+	assert main(Boston) == "Boston"
+	assert main(school) == "school"
+	assert main(family) == "family"
+	assert main(coffee) == "coffee"
